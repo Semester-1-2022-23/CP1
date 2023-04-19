e4482f4e44beacf6511ae82c1b8e2b781f3cfdae
ATemova <119525418+ATemova@users.noreply.github.com>
2023-04-19 16:54:00 +0200

    Create exe11.c

---
+++ exe11.c
@@ -0,0 +1,41 @@
+/*Write a program in C to count a total number of duplicate elements in an array. Minimum should
+be 5 and maximum is 25 elements. Inputs must be integers (both negative and positive).*/
+#include <stdio.h>
+
+int main()
+{
+    int dLength;
+    printf("Enter the length of the array: ");
+    scanf("%d", &dLength);
+
+    int dArr[dLength];
+    char dTemp[dLength];
+
+    for (int i = 0; i < dLength; i++)
+    {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &dArr[i]);
+        dTemp[i] = (char)dArr[i];
+    }
+
+    int dCount1 = 0;
+    int dCount2 = 0;
+
+    for (int i = 0; i < dLength; i++){
+        if (dArr[i] != -00){
+            dCount1 = 0;
+            for (int j = i + 1; j < dLength; j++){
+                if (dArr[i] == dArr[j]){
+                    dCount1++;
+                    dArr[j] = -00;
+                }
+            }
+            if (dCount1 > 0){
+                dCount2++;
+            }
+            dArr[i] = 321;
+        }
+    }
+
+    printf("%d\n", dCount2);
+}
